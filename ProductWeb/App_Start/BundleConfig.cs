d1804c6d9e4714a88e2978f2e15855f8b2740b03
James Robert Perih <james.perih@brewninja.net>
2018-12-21 11:37:46 -0600

    Resolve Web Bundling issues. Remove extraneous Web code.

--- ProductWeb/App_Start/BundleConfig.cs
+++ ProductWeb/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace ProductWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
